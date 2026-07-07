f66758af5779f205a67c22cfbd90fc6737a65c57
YerdosNar <besway2000@gmail.com>
2026-07-07 20:31:42 +0900

    refactor: Updated 'run.sh' script to show help

--- clang/run.sh
+++ clang/run.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 1 ]; then
-    read -p "Which one (T)est, (M)NIST: " choice
+        read -p "Which one (T)est, (M)NIST: " choice
 
-    if [ "$choice" == "M" ] || [ "$choice" == "m" ]; then
-        gcc -o mnist mnist_img.c kmeans.c -lm -lSDL2 -O3
-        ./mnist
-        rm mnist
-    elif [ "$choice" == "T" ] || [ "$choice" == "t" ]; then
-        gcc -o test test.c kmeans.c -lm -lSDL2 -O3
-        ./test
-        rm test
-    else
-        echo "Invalid input"
-    fi
+        if [ "$choice" == "M" ] || [ "$choice" == "m" ]; then
+                gcc -o mnist mnist_img.c kmeans.c -lm -lSDL2 -O3
+                ./mnist
+                rm mnist
+        elif [ "$choice" == "T" ] || [ "$choice" == "t" ]; then
+                gcc -o test test.c kmeans.c -lm -lSDL2 -O3
+                ./test
+                rm test
+        else
+                echo "Invalid input"
+        fi
 else
-    if [ "$1" == "M" ] || [ "$1" == "mnist" ] || [ "$1" == "m" ]; then
-        gcc -o mnist mnist_img.c kmeans.c -lm -lSDL2 -O3
-        ./mnist
-        rm mnist
-    elif [ "$1" == "T" ] || [ "$1" == "test" ] || [ "$1" == "t" ]; then
-        gcc -o test test.c kmeans.c -lm -lSDL2 -O3
-        ./test
-        rm test
-    else
-        echo "Invalid argument"
-    fi
+        if [ "$1" == "-M" ] || [ "$1" == "--mnist" ] || [ "$1" == "-m" ]; then
+                gcc -o mnist mnist_img.c kmeans.c -lm -lSDL2 -O3
+                ./mnist
+                rm mnist
+        elif [ "$1" == "-T" ] || [ "$1" == "--test" ] || [ "$1" == "-t" ]; then
+                gcc -o test test.c kmeans.c -lm -lSDL2 -O3
+                ./test
+                rm test
+        elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+                echo "$0 [options]"
+                echo ""
+                echo "Options:"
+                echo "          -M/-m/--mnist       to run mnist images"
+                echo "          -T/-t/--test        to run custom test"
+        else
+                echo "Invalid argument"
+        fi
 fi
 
